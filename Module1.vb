2c8ce94611f725e775f027895277f0928f57d8d5
Ferguzus <r99993140@gmail.com>
2024-02-24 18:34:16 +0800

    Added initial files

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+Module Module1
+    Public con As New MySqlConnection
+    Public cmd As New MySqlCommand
+    Public adapter As New MySqlDataAdapter
+    Public data As New DataSet
+    Public table As New DataTable
+
+    Sub openCon()
+        ' Set the connection string first
+        con.ConnectionString = "server=localhost;username=root;password=12345;database=db_tutorial"
+
+        ' Open the connection
+        con.Open()
+    End Sub
+End Module
